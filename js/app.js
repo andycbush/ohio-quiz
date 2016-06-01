9c46501284cd60ec933ac70d922bdfe6204820c5
Andy Bush <andycbush@yahoo.com>
2016-06-01 17:28:06 -0400

    initial commit
    first session on quiz app

---
+++ js/app.js
@@ -0,0 +1,29 @@
+//define global variables
+
+var questionsArray = [
+    //question 1
+    {
+        questionText: 'How many US presidents are from the state of Ohio?',
+        questionChoices: [4, 5, 6],
+        questionCorrectChoice: 2,
+        correctDetails: 'Ohio is credited with providing 6 presidents, more than any other state.'
+    },
+    //question 2
+    {
+        questionText: 'What is the official state fruit?',
+        questionChoices: ['apple', 'tomato', 'cherry'],
+        questionCorrectChoice: 1,
+        questionDetails: 'The tomato is the official state fruit!'
+    },
+    //question 3
+    {
+        questionText: 'What is the official state rock and roll song?',
+        questionChoices: ['Go Buckeyes!', 'Hang on Sloopy', 'Beautiful Ohio'],
+        questionCorrectChoice: 1,
+        questionDetails: 'Hang on Sloopy is the official rock and roll song!'
+}
+];
+
+//define functions
+
+//use functions
